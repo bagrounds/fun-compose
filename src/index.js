9443871505c5c2c586b05db64af5e32720d8a628
bagrounds <bagrounds@gmail.com>
2017-01-04 23:51:16 -0800

    [PATCH][FIX] input and output spec application, add tests

--- src/index.js
+++ src/index.js
@@ -10,18 +10,23 @@
   var outputSpec = require('./spec-output')
 
   /* exports */
-  module.exports = compose(inputSpec, compose, outputSpec)
+  module.exports = compose
 
   /**
    *
    * @return {Function} the composition of the input functions
    */
   function compose () {
-    return Array.prototype.slice.call(arguments).reduce(function (f1, f2) {
-      return function () {
-        return f2(f1.apply(null, arguments))
-      }
-    })
+    inputSpec.apply(null, arguments)
+
+    var composed = Array.prototype.slice.call(arguments)
+      .reduce(function (f1, f2) {
+        return function () {
+          return f2(f1.apply(null, arguments))
+        }
+      })
+
+    return outputSpec(composed)
   }
 })()
 
--- test/tests.js
+++ test/tests.js
@@ -20,8 +20,38 @@
       result: funAssert.type('Function'),
       sync: true,
       transformer: funTransform.argsToObject(['a', 'b'])
+    },
+    {
+      input: {
+        a: 3,
+        b: 4
+      },
+      result: funAssert.equal(49),
+      sync: true,
+      transformer: function sumThenSquare (funCompose) {
+        return funTransform.argsToObject(['a', 'b'])(
+          funCompose(sum, square)
+        )
+      }
+    },
+    {
+      input: {
+        a: 3,
+        b: 4
+      },
+      result: funAssert.equal(50),
+      sync: true,
+      transformer: function sumThenSquarePlusOne (funCompose) {
+        return funTransform.argsToObject(['a', 'b'])(
+          funCompose(sum, square, plusOne)
+        )
+      }
     }
   ].map(funTest)
+
+  function plusOne (number) {
+    return number + 1
+  }
 
   function sum () {
     return Array.prototype.slice.call(arguments).reduce(function (a, b) {
